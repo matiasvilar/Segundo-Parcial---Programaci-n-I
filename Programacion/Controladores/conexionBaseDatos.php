3ca37cf6e7d347fb0b1904eba84eb53651de22cc
Matías Vilar <45578107+matiasvilar@users.noreply.github.com>
2022-07-09 00:01:29 -0300

    Delete Programacion directory

--- Programacion/Controladores/conexionBaseDatos.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-$conexion = mysqli_connect('localhost','root','','db');
-mysqli_set_charset($conexion,"utf8");
-if($conexion -> connect_error){
-	echo "Ups, hay problemas con la base de datos";
-	exit;
-}
-?>
