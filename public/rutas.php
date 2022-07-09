357afcda23d61f7c98198d0e2a9e05878d11272a
Matías Vilar <45578107+matiasvilar@users.noreply.github.com>
2022-07-08 22:27:05 -0300

    Delete public directory

--- public/rutas.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    require "../utils/autoload.php";
-
-    Routes::AddView("/login","login");
-    Routes::Add("/login","post","SesionControlador::IniciarSesion");
-    
-    Routes::Run();
-
-       
